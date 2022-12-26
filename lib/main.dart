f89c6e88159998ded85c17a2ec6fde74992d1e22
batuhanyrs <batuhanyaras@yahoo.com>
2022-12-26 21:32:01 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,25 +47,6 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Card(
-              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal.shade800,
-                ),
-                title: Text(
-                  '+905*********',
-                  style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.teal.shade800,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15.0,
-                    letterSpacing: 0.5,
-                  ),
-                ),
-              ),
-            ),
-            Card(
               margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
               child: ListTile(
                 leading: Icon(
@@ -73,6 +55,44 @@ class MyApp extends StatelessWidget {
                 ),
                 title: Text(
                   'batuhanyaras@yahoo.com',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15.0,
+                    letterSpacing: 0.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade800,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.linkedin,
+                  color: Colors.teal.shade800,
+                ),
+                title: Text(
+                  'linkedin.com/in/batuhanyaras',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15.0,
+                    letterSpacing: 0.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade800,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.github,
+                  color: Colors.teal.shade800,
+                ),
+                title: Text(
+                  'https://github.com/batuhan-yaras',
                   style: TextStyle(
                     fontFamily: 'SourceSansPro',
                     fontSize: 15.0,
